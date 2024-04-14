6daa86012c9f807b1ff12bf8e3b24a4cf12767a8
LaurentLienhard <laurent.lienhard@outlook.com>
2024-04-14 16:47:03 +0000

    update

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,7 +5,7 @@ git config --global user.name "LaurentLienhard"
 git config --global --add safe.directory /workspaces/MyLab
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install inetutils-ping sshpass direnv -y
+sudo apt install inetutils-ping sshpass direnv vim -y
 curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh font install CascadiaCode
 eval "$(oh-my-posh --init --shell bash --config ~/.kali.omp.json)"
