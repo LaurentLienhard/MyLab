26969189c8764b2696f1b5b294215977679bfb9e
LaurentLienhard <laurent.lienhard@outlook.com>
2024-05-04 20:22:35 +0000

    update

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,7 +5,11 @@ git config --global user.name "LaurentLienhard"
 git config --global --add safe.directory /workspaces/MyLab
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt install inetutils-ping sshpass direnv packer vim -y
+sudo apt install inetutils-ping sshpass direnv wget vim software-properties-common xorriso -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install packer
+
 curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh font install CascadiaCode
 eval "$(oh-my-posh --init --shell bash --config ~/.kali.omp.json)"
